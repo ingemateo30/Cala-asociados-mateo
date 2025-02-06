51e27fb9a9dea4722f0b0511a053d9ff6a6c2fc2
ingemateo30 <msalazar5@udi.edu.co>
2025-02-06 11:11:38 -0500

    sin usuario

--- src/app/routes/paths.js
+++ src/app/routes/paths.js
@@ -7,7 +7,7 @@ export const paths = {
       nosotros: `/nosotros`,
       servicios: '/servicios',
       noticias: '/noticias',
-      nuestroEquipo: '/nuestroequipo',
+      //nuestroEquipo: '/nuestroequipo',
       contacto: '/contacto',
     },
   };
