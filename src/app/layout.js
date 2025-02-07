7586e7e3a07d32d0bda725dd30d7f81a9a740164
ingemateo30 <msalazar5@udi.edu.co>
2025-02-06 21:50:54 -0500

    imagen24

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es" className={primaryFont.className}>
+      <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
       <body className="background-image">
         <div className="content">
           {children}
