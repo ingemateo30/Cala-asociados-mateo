071ed92d73dd03aa84acf2d076baa956f86132b0
ingemateo30 <msalazar5@udi.edu.co>
2025-02-07 08:35:15 -0500

    imagen37

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
      <html lang="es" className={primaryFont.className}>
-
       <body>{children}</body>
     </html>
   );
